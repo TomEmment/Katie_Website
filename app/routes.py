24f15b6627436a86f143e70b3f91390b1bb145ef
TomEmment <N0789683@my.ntu.ac.uk>
2022-04-29 17:30:52 +0100

    Set up

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,22 @@ import csv
 # -------- Visable routes
 @app.route('/')
 def index():
-    return "You Shouldnt be here"
+    return render_template('home.html')
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/gallery_concept')
+def gallery1():
+    return render_template('gallery_concept.html')
+
+@app.route('/gallery_vector')
+def gallery2():
+    return render_template('gallery_vector.html')
 
 
