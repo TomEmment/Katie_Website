6cf077b5f5f22e65a8611f27f11ef562184aa0f3
TomEmment <thomas.emment2018@my.ntu.ac.uk>
2022-05-11 17:05:53 +0100

    Added openseadragon to vector gallery

---
+++ app/static/js/gallery_vector.js
@@ -0,0 +1,50 @@
+const imgBtn = document.querySelector('.img-btn')
+imgBtn.addEventListener('click', loadImage)
+
+const imgBtn1 = document.querySelector('.img-btn1')
+imgBtn1.addEventListener('click', loadImage)
+
+const imgBtn2 = document.querySelector('.img-btn2')
+imgBtn2.addEventListener('click', loadImage)
+
+const imgBtn3 = document.querySelector('.img-btn3')
+imgBtn3.addEventListener('click', loadImage)
+
+const imgBtn4 = document.querySelector('.img-btn4')
+imgBtn4.addEventListener('click', loadImage)
+
+const imgBtn5 = document.querySelector('.img-btn5')
+imgBtn5.addEventListener('click', loadImage)
+
+const imgBtn6 = document.querySelector('.img-btn6')
+imgBtn6.addEventListener('click', loadImage)
+
+const imgBtn7 = document.querySelector('.img-btn7')
+imgBtn7.addEventListener('click', loadImage)
+
+const imgBtn8 = document.querySelector('.img-btn8')
+imgBtn8.addEventListener('click', loadImage)
+
+const imgBtn9 = document.querySelector('.img-btn9')
+imgBtn9.addEventListener('click', loadImage)
+
+const imgBtn10 = document.querySelector('.img-btn10')
+imgBtn10.addEventListener('click', loadImage)
+
+function loadImage(e){
+    $('#modalContent').html('');
+
+
+    var img = OpenSeadragon({
+        id: 'modalContent',
+        defaultZoomLevel: 1,
+        minZoomLevel: 0.7,
+        prefixUrl: "../static/js/images/",
+        tileSources:{
+            type: 'image',
+            url: e.target.src,
+            Format: 'jpg',
+            buildPyramid: false,
+        },
+    })
+}
